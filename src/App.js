b246f510f50f3f3b757aac65e5860686efe1735a
Alfredo08 <alfredosv08@gmail.com>
2021-04-22 14:14:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import React, {useState} from 'react';
 
 function App(){
-	const [drinks, setDrinks] = useEffect([]);
+    const [drinks, setDrinks] = useState([]);
 
 
     return(
       <div>
-        {/* Build up a form that connects to thecocktaildb API */}
-        {/* The form only requires a text field so that the user can input the name of a drink */}
-        {/* On the submission of the form you must connect to the API, grab the information and update the array of drinks in the state*/}
+        {/* Build up a form that connects to thecocktaildb API: https://www.thecocktaildb.com/api.php */}
+        {/* The form only requires a text field so that the user can input the name of a drink and a button for submission */}
+        {/* On the submission of the form you must connect to the API, grab the information with the search criteria and update the array of drinks in the state*/}
       
         {/* Display the array of drinks in the page. Place just the name of the drink and an image*/}
       </div>
